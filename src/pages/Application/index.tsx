869ab931928203f43244434f542c50933931fd6f
Mehroj <mehrojhamroev261@gmail.com>
2026-03-23 11:11:57 +0500

    update444

---
+++ src/pages/Application/index.tsx
@@ -0,0 +1,11 @@
+const Application = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Application Page
+      </h1>
+    </div>
+  );
+};
+
+export default Application;
